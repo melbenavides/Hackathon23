a227cf7874b0deafa2326133ef4cf00a384b8342
pabloramos24 <131503290+pabloramos24@users.noreply.github.com>
2023-04-21 23:21:29 -0600

    Add files via upload

---
+++ Site/script.js
@@ -0,0 +1,14 @@
+var content = document.getElementById('busca_abogado_content');
+var content_two = document.getElementById('documentos_content');
+var content_three = document.getElementById('materia_civil_content');
+function toggle() {
+    content.classList.toggle('toggle');
+}
+
+function toggle_two() {
+    content_two.classList.toggle('toggle');
+}
+
+function toggle_three() {
+    content_three.classList.toggle('toggle');
+}
